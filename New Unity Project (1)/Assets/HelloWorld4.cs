9838b483d70fc9cf020cc4994723674d3803d7b3
yeongchan1228 <79392304+yeongchan1228@users.noreply.github.com>
2021-06-17 02:52:53 +0900

    second
    second

---
+++ New Unity Project (1)/Assets/HelloWorld4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("¿Ã∞‘ ø÷ æ»µ ????");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
