89f7d44d11b43815c3a5a688528dae42214af27d
Cl8D <46810837+Cl8D@users.noreply.github.com>
2021-06-17 03:06:41 +0900

    hhihiihihi

--- New Unity Project (1)/Assets/HelloWorld2.cs
+++ New Unity Project (1)/Assets/HelloWorld2.cs
@@ -7,7 +7,7 @@ public class HelloWorld2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hi");
+        print("hihihihihhihihihhiihi");
     }
 
     // Update is called once per frame
