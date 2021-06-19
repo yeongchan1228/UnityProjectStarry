bbaabcca4496c3847732cd70261d5e33524d8c20
yeongchan1228 <79392304+yeongchan1228@users.noreply.github.com>
2021-06-19 16:44:23 +0900

    마을 생성

--- New Unity Project (1)/Assets/mapScript/CharacterMove.cs
+++ New Unity Project (1)/Assets/mapScript/CharacterMove.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld5 : MonoBehaviour
+public class CharacterMove : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print("1ÀÏ Â÷ ³¡");
+        
     }
 
     // Update is called once per frame
