1c5b35a3ff16bc16a36dcf04af05b204adfff822
soongsilmaru <asdbnm066@daum.net>
2021-06-20 23:05:53 +0900

    애니추가

--- New Unity Project (1)/Assets/Scrpits/PlayerController.cs
+++ New Unity Project (1)/Assets/Scrpits/PlayerController.cs
@@ -6,14 +6,17 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float startX, startY;
     private Rigidbody2D rigid2D;
     Vector3 viewpos;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid2D = GetComponent<Rigidbody2D>();
-        transform.position = new Vector3(-17.5f, 16, 0);
+        transform.position = new Vector3(startX, startY, 0);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,10 +28,9 @@ public class PlayerController : MonoBehaviour
         
         //transform.position += new Vector3(x, y, 0) * moveSpeed * Time.deltaTime;
         rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
     }
-
-
-
-   
    
 }
