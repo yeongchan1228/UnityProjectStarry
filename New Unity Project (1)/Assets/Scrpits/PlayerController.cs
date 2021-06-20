2bf5859d5b7db7cba6cdef32e99765f752937046
Cl8D <46810837+Cl8D@users.noreply.github.com>
2021-06-20 18:51:49 +0900

    여캐움직여요

--- New Unity Project (1)/Assets/Scrpits/PlayerController.cs
+++ New Unity Project (1)/Assets/Scrpits/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(-3, 0, 0);
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,5 +27,9 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
         }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+
     }
 }
