e41b5fd00455c1555f765ebd19fd00089090bc64
yeongchan1228 <79392304+yeongchan1228@users.noreply.github.com>
2021-06-20 16:44:02 +0900

    메인 카메라 캐릭터에 따라 이동

--- New Unity Project (1)/Assets/Scrpits/PlayerController.cs
+++ New Unity Project (1)/Assets/Scrpits/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
+        transform.position = new Vector3(-3, 0, 0);
     }
 
     // Update is called once per frame
