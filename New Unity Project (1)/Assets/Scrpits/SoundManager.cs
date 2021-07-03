550fe445d2023289e1c6f69a76b9e4a002f429fb
soongsilmaru <asdbnm066@daum.net>
2021-07-03 23:59:04 +0900

    볼륨조절 / 상점 나누기

---
+++ New Unity Project (1)/Assets/Scrpits/SoundManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    float volume;
+    public AudioSource audioSource;
+    public Slider volslider;
+    void Start()
+    {
+        volslider.onValueChanged.AddListener(SetAudioVol);
+    }
+
+    public void SetAudioVol(float vol)
+    {
+        audioSource.volume = vol;
+    }
+}
